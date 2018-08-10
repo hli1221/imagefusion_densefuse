1fc5be62d10dc673e451a70a0b75233e4d6b3080
HuiLi <hui_li_jnu@163.com>
2018-08-10 11:59:25 +0800

    main

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ import os
 
 # IS_TRAINING = True
 IS_TRAINING = False
-IS_VIDEO = True
+IS_VIDEO = False
 
 BATCH_SIZE = 2
 EPOCHES = 4
 
 SSIM_WEIGHTS = [1, 10, 100, 1000]
 MODEL_SAVE_PATHS = [
-    'D:/project/GitHub/ImageFusion/imagefusion_deep_dense_block/models/deepfuse_dense_model_bs2_epoch4_all_weight_1e0.ckpt',
-    'D:/project/GitHub/ImageFusion/imagefusion_deep_dense_block/models/deepfuse_dense_model_bs2_epoch4_all_weight_1e1.ckpt',
-    'D:/project/GitHub/ImageFusion/imagefusion_deep_dense_block/models/deepfuse_dense_model_bs2_epoch4_all_weight_1e2.ckpt',
-    'D:/project/GitHub/ImageFusion/imagefusion_deep_dense_block/models/deepfuse_dense_model_bs2_epoch4_all_weight_1e3.ckpt',
+    './models/models/densefuse_gray/densefuse_model_bs2_epoch4_all_weight_1e0.ckpt',
+    './models/models/densefuse_gray/densefuse_model_bs2_epoch4_all_weight_1e1.ckpt',
+    './models/models/densefuse_gray/densefuse_model_bs2_epoch4_all_weight_1e2.ckpt',
+    './models/models/densefuse_gray/densefuse_model_bs2_epoch4_all_weight_1e3.ckpt',
 ]
 
 # MODEL_SAVE_PATH = './models/deepfuse_dense_model_bs4_epoch2_relu_pLoss_noconv_test.ckpt'
@@ -36,11 +36,12 @@ def main():
 
 	if IS_TRAINING:
 
-		original_imgs_path = list_images('D:/ImageDatabase/Image_fusion_MSCOCO/original/')
+		original_imgs_path = list_images('D:/Database/Image_fusion_MSCOCO/original/')
+		validatioin_imgs_path = list_images('./validation/')
 
 		for ssim_weight, model_save_path in zip(SSIM_WEIGHTS, MODEL_SAVE_PATHS):
 			print('\nBegin to train the network ...\n')
-			train_recons(original_imgs_path, model_save_path, model_pre_path, ssim_weight, EPOCHES, BATCH_SIZE, debug=True)
+			train_recons(original_imgs_path, validatioin_imgs_path, model_save_path, model_pre_path, ssim_weight, EPOCHES, BATCH_SIZE, debug=True)
 
 			print('\nSuccessfully! Done training...\n')
 	else:
@@ -55,19 +56,19 @@ def main():
 			         ssim_weight, 0, IS_VIDEO, 'addition', output_path=output_save_path)
 		else:
 			print('\nBegin to generate pictures ...\n')
-
-			path = 'images/IV_images/'
-			for i in range(20):
+			is_RGB = True
+			path = 'images/color/'
+			for i in range(3):
 				index = i + 1
-				infrared = path + 'IR' + str(index) + '.png'
-				visible = path + 'VIS' + str(index) + '.png'
+				infrared = path + 'lytro-' + str(index) + '-A.jpg'
+				visible = path + 'lytro-' + str(index) + '-B.jpg'
 				fusion_type = 'addition'
 				# fusion_type = 'l1'
 				for ssim_weight, model_path in zip(SSIM_WEIGHTS, MODEL_SAVE_PATHS):
-					output_save_path = 'outputs/fused_deepdense_bs2_epoch4_all_l1_focus_'+str(ssim_weight)
+					output_save_path = 'outputs/color'
 
 					generate(infrared, visible, model_path, model_pre_path,
-					         ssim_weight, index, IS_VIDEO, type = fusion_type, output_path = output_save_path)
+					         ssim_weight, index, IS_VIDEO, is_RGB, type = fusion_type, output_path = output_save_path)
 
 
 if __name__ == '__main__':
